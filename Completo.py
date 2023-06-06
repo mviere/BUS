595dd92813320f1996c894eadf1020b027b59501
Piru <fabianabigi@MacBook-Air-de-Fabiana.local>
2023-06-06 13:40:04 -0300

    resolucion de bug en house keeping y del main

--- Completo.py
+++ Completo.py
@@ -60,11 +60,11 @@ def DF2CSV (df: pd.DataFrame, filename: str, estilo: str):
     return
 
 
-def mode_transition(deftable_filename: str, dT: int):
+def mode_transition (deftable_transicion: str, dT: int):
 
     command_queue_trans = []
 
-    df_deftable = CSV2DF(deftable_filename)
+    df_deftable = CSV2DF(deftable_transicion)
     
     for cmd in df_deftable.values.tolist():
         for c in cmd:
@@ -79,44 +79,39 @@ def mode_transition(deftable_filename: str, dT: int):
     return command_queue_trans
 
 
-def mode_processing(deftable_filename: str, deftable_filename3: str, modo: str):
+def mode_processing(deftable_modos: str, deftable_housekeeping: str, mode_name: str):
     
     commmand_queue = []
     duracion = []
 
-    df_deftable = CSV2DF(deftable_filename)
-    df_deftable3 = CSV2DF(deftable_filename3)
+    df_deftable = CSV2DF(deftable_modos)
+    df_deftable3 = CSV2DF(deftable_housekeeping)
 
-    df_blockcmd = df_deftable.loc[(df_deftable["Modo"] == modo), :]
-    df_blockcmd3 = df_deftable3.loc[(df_deftable["Modo"] == modo), :]
+    df_blockcmd = df_deftable.loc[(df_deftable["Modo"] == mode_name), :]
+    df_blockcmd3 = df_deftable3.loc[(df_deftable3["Modo"] == mode_name), :]
 
     for cmd in df_blockcmd['Comandos'].values.tolist():
         commmand_queue.append(cmd)
     for t in df_blockcmd['Duracion'].values.tolist():
         duracion.append(t)
     
-    ### TENGO PROBLEMAS ACÁ, NO PUEDO CREAR EL FUKIN DATAFRAME
-
-    # PASO 1: Creo DataFrame con los valores nominales de las variables de housekeeping
-    sensor_list = df_blockcmd3["Sensor"].values.tolist()
-    temp_min_list = df_blockcmd3["Temperatura min [C]"].values.tolist()
-    temp_max_list = df_blockcmd3["Temperatura max [C]"].values.tolist()
-    
-    index = pd.Index(sensor_list, name="Sensor")
-    df_nominalvalues = pd.DataFrame({'Temperatura min. [C]': temp_min_list, 'Temperatura max. [C]': temp_max_list}, index=index)
+    # PASO 1 (cont): Creo DataFrame con los valores nominales de las variables de housekeeping    
+    index = pd.Index(df_blockcmd3["Sensor"].values, name="Sensor")
+    df_nominalvalues = pd.DataFrame({'Temperatura min. [C]': df_blockcmd3["Temperatura min [C]"].values, 'Temperatura max. [C]': df_blockcmd3["Temperatura max [C]"].values}, index=index)
 
     return commmand_queue, duracion, df_nominalvalues
 
 
-def mode_management (df_mode: pd.DataFrame, deftable_filename: str, deftable_filename2: str, deftable_filename3: str, cant_T: int, dT: int) -> list:
-
+def mode_management (mode_name: str, cant_ciclos: int, dT: int, deftable_modos: str, deftable_transicion: str, deftable_housekeeping: str) -> list:
+    """
+    Esta función
+    """
     command_queue = []
 
-    command_queue_trans = mode_transition(deftable_filename2, dT)
+    command_queue_trans = mode_transition(deftable_transicion, dT)
     command_queue.extend(command_queue_trans)
    
-    modo = df_mode["Modo"].values[0]
-    command_queue2, lista_duracion, df_nominalvalues = mode_processing(deftable_filename, deftable_filename3, modo)
+    command_queue2, lista_duracion, df_nominalvalues = mode_processing(deftable_modos, deftable_housekeeping, mode_name)
     
     for ciclo in range(cant_T):
         for i in range(len(command_queue2)):
@@ -131,10 +126,10 @@ def mode_management (df_mode: pd.DataFrame, deftable_filename: str, deftable_fil
 def command_processing (df_nominalvalues: pd.DataFrame, command_queue: list, timer) -> None:
     
     # PASO 3: le paso los valores nominales a application_management para ver en qué estado está el housekeeping
-    df_housekeeping = application_management(housekeeping = df_nominalvalues)
+    df_housekeeping = application_management(df_nominalvalues) #df_housekeeping = application_management(housekeeping = df_nominalvalues)
 
     # PASO 7: imprimo el estado de housekeeping
-    print("\nHouse Keeping State:\n", df_housekeeping)
+    print("\nHouse Keeping\n\n", df_housekeeping)
 
     for cmd in command_queue:
         t = timer.get_current_time()
@@ -145,7 +140,7 @@ def command_processing (df_nominalvalues: pd.DataFrame, command_queue: list, tim
     return
 
 
-def event_handling(event: str, regist_filename: str, deftable_eventos: str, dT: int, timer) -> list:
+def event_handling(event: str, event_register: str, deftable_eventos: str, dT: int, timer) -> list:
     command_queue_event = []
 
     df_deftable = CSV2DF(deftable_eventos)
@@ -154,10 +149,9 @@ def event_handling(event: str, regist_filename: str, deftable_eventos: str, dT: 
     command_queue_event = df_blockcmd['Comandos'].values.tolist()
 
     t = timer.get_current_time()
-    new_row = pd.DataFrame({'Descripcion': [df_blockcmd["Evento"].values[0]], 'Cualk': [t]})
+    new_row = pd.DataFrame({'Descripcion': [df_blockcmd["Evento"].values[0]], 'Tiempo Universal': [t]})
     df_evento = pd.DataFrame(new_row)
-
-    DF2CSV(df_evento, regist_filename, 'append')
+    DF2CSV(df_evento, event_register, 'append')
 
     while len(command_queue_event) < dT:
         command_queue_event.append(None)
@@ -165,12 +159,13 @@ def event_handling(event: str, regist_filename: str, deftable_eventos: str, dT: 
     return command_queue_event
 
 
-def application_management (**kwargs):
+'''def application_management (**kwargs):
     """
     Esta función ahora mismo solo reporta el estado de un sensor de temperatura, pero se debería ingresar con una opción y sus inputs
     para que realice más tareas que el housekeeping. Por ejemplo, si fuera a activar actuadores lo haría desde aquí.
     """
-    if kwargs.get('housekeeping') != None:
+    print(type(kwargs.get('housekeeping')))
+    if kwargs.get('housekeeping') != :
 
         df_nominalvalues = kwargs.get('housekeeping')
 
@@ -190,23 +185,51 @@ def application_management (**kwargs):
         
         return df_housekeeping
     else:
-        return 
+        return '''
+
+
+def application_management (df_nominalvalues: pd.DataFrame):
+    """
+    Esta función ahora mismo solo reporta el estado de un sensor de temperatura, pero se debería ingresar con una opción y sus inputs
+    para que realice más tareas que el housekeeping. Por ejemplo, si fuera a activar actuadores lo haría desde aquí.
+    """
+
+    # PASO 4: obtengo los estados de las variables representativas del housekeeping
+    df_realvalues = sensing_temperature(df_nominalvalues.index.values)
+
+    # PASO 5: comparo los valores nominales de las variables representativas con los estados de las variables
+    state_list = []
+
+    i = 0
+    for sensor, cols in df_realvalues.iterrows():
+        if df_nominalvalues['Temperatura min. [C]'].values[i] < df_realvalues['Temperatura [C]'].values[i] < df_nominalvalues['Temperatura max. [C]'].values[i]:
+            state_list.append('Out of range')
+        else:
+            state_list.append('In range')
+        i = i + 1
+            
+    # PASO 6: creo el DataFrame que reporta el estado de un sensor: 'Out of range' o 'In range'
+    index = pd.Index(df_nominalvalues.index.values, name='Sensor')
+    df_housekeeping = pd.DataFrame({'Estado': state_list}, index=index)
+        
+    return df_housekeeping
+
 
 # %% Application
 # ---------------------------------------------------------------------------- #
 
-def sensing_temperature (sensors_list):
+def sensing_temperature (sensors):
     """
     Esta función genera el registro del estado de las variables representativas del housekeeping.
     """
     temp_list = []
 
-    for sensor in sensors_list:
-        temp = temp_sensors(sensor)
+    for sensor in sensors:
+        temp = temp_sensor(sensor)
         temp_list.append(temp)
 
     # Creo el DataFrame de Housekeeping
-    index = pd.Index(sensors_list, name="Sensor")
+    index = pd.Index(sensors, name='Sensor')
     df_realvalues = pd.DataFrame({'Temperatura [C]': temp_list}, index=index)
 
     return df_realvalues
@@ -232,51 +255,54 @@ def actuators ():
 
 def main():
     
-    print("\nImportando librerías y scripts necesarios...\n")
-
-    escenario_filename = "escenario.csv"
+    # Información del archivo "escenario.csv".
+    escenario_filename = "escenario.csv"                #Establece una intención de secuencia de modos y su permanencia
     df_escenario = CSV2DF(escenario_filename)
 
+    # Definición de granularidades temporales
     dT = 5
     dt = 1
     
-    deftable_filename = "deftable_modos.csv"
-    deftable_filename2 = "deftable_transicion.csv"
-    deftable_filename3 = "deftable_housekeeping.csv"
+    # Rutas a archivos auxiliares
+    deftable_modos = "deftable_modos.csv"            #Def. Table con Bloque de Comandos para cada modo
+    deftable_transicion = "deftable_transicion.csv"      #Def. Table con Bloque de Comandos para la transición entre modos
+    deftable_housekeeping = "deftable_housekeeping.csv"    #Def. Table con Bloque de Comandos para el housekeeping de cada modo
+    deftable_eventos = "deftable_eventos.csv"           #Def. Table con Bloque de Comandos para el manejo de eventos impredecibles
+    event_register = "event_register.csv"              
+    
+    # Creación del DataFrame donde se almacenarán los eventos ocurridos
+    df = pd.DataFrame(columns=['Descripcion', 'Tiempo Universal'])
+    DF2CSV(df, event_register, 'write')
 
-    deftable_eventos = "deftable_eventos.csv"
-    regist_filename = "event_register.csv"
-    df_evento1 = pd.DataFrame(columns=['Descripcion', 'Cualk'])
-    DF2CSV(df_evento1, regist_filename, 'write')
-
+    # Inicio del reloj
     timer = TimerThread()
     timer.start()
 
-
-    
     endProgram = False
     while not endProgram:
         
         for index, cols in df_escenario.iterrows():
             
-            df_mode = df_escenario.loc[[index], :]
-            ciclos = df_mode['Ciclos'].values[0]
-            # PASO 1: obtengo los valores nominales de las variables representativas de housekeeping del modo
-            command_queue, df_nominalvalues = mode_management(df_mode, deftable_filename, deftable_filename2, deftable_filename3, ciclos, dT)
-            
             event = bool(random.getrandbits(1))
             if event:
                 event = random.choice(["Perdida de SMA por drag", "Perturbaciones por gravedad"])
-
-                command_queue = event_handling(event, regist_filename, deftable_eventos, dT, timer)
-
+                command_queue = event_handling(event, event_register, deftable_eventos, dT, timer)
                 command_processing(df_nominalvalues, command_queue, timer)
-
-            # PASO 2: paso los valores nominales a commando processing para obtener el estado de housekeeping allí
-            command_processing(df_nominalvalues, command_queue, timer)
+            else:
+                df_mode = df_escenario.loc[[index], :]
+                cant_ciclos = df_mode['Ciclos'].values[0]
+                mode_name = df_mode['Modo'].values[0]
+                
+                # PASO 1: obtengo los valores nominales de las variables representativas de housekeeping del modo
+                command_queue, df_nominalvalues = mode_management(df_mode, deftable_modos, deftable_transicion, deftable_housekeeping, ciclos, dT)
+                mode_management (mode_name, cant_ciclos, dT, deftable_modos, deftable_transicion, deftable_housekeeping)
+                
+                # PASO 2: paso los valores nominales a commando processing para obtener el estado de housekeeping allí
+                command_processing(df_nominalvalues, command_queue, timer)
 
         endProgram = True
 
+    # Fin del reloj
     timer.stop()
     timer.join()
 
